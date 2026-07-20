0bd2d81d4733b32e9b07a62c86f7560d07f9dc54
RAKOTOMILIARISON Lucas <lucasrakotomily@gmail.com>
2026-07-20 10:29:51 +0300

    [feat] view login avec validation js

---
+++ app/Views/authClient/login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+
+<body>
+    <form action="<?= base_url('doLogin') ?>" method="post">
+        <label for="numero">Numéro de téléphone:</label>
+        <input type="text" name="numero" id="numero" required>
+        <button type="submit">Se connecter</button>
+    </form>
+</body>
+
+</html>
+<script>
+    let submitButton = document.querySelector('button[type="submit"]');
+    submitButton.addEventListener('click', function (event) {
+        event.preventDefault();
+        let numeroInput = document.getElementById("numero");
+        let numero = numeroInput.value;
+        const motif = "^(?:\\+261|0)\\s?(20|32|33|34|37|38|39)(?:[\\s]?\\d{2})(?:[\\s]?\\d{3})(?:[\\s]?\\d{2})$";
+        let regex = new RegExp(motif);
+        if (regex.test(numero)) {
+            document.getElementById("numero").style.borderColor = "green";
+            this.form.submit();
+        } else {
+            document.getElementById("numero").style.borderColor = "red";
+        }
+    });
+
+</script>
