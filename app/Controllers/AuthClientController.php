644c7e11278b61e184b85c1d1dc1351e20e26de1
RAKOTOMILIARISON Lucas <lucasrakotomily@gmail.com>
2026-07-20 10:16:12 +0300

    AuthClietn Controller

---
+++ app/Controllers/AuthClientController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use App\Models\ClientModel;
+
+class AuthClientController extends BaseController
+{
+    function login() {
+        if (session()->get('isLoggedIn')) {
+            return redirect()->to('/home');
+        }
+        return view('authClient/login');
+    }
+    function doLogin()
+    {
+        $numero = $this->request->getPost('numero');
+        $clientModel = new ClientModel();
+
+        $motif = "^(?:\\+261|0)\\s?(20|32|33|34|37|38|39)(?:[\\s]?\\d{2})(?:[\\s]?\\d{3})(?:[\\s]?\\d{2})$";
+
+        if (preg_match($motif, $numero)) {
+            if (str_starts_with($numero, '+261')) {
+                $numero = substr($numero, 4);
+            }
+            $client = $clientModel->where('numero', $numero)->first();
+            if ($client) {
+                session()->regenerate(true);
+                session()->setFlashdata('success', 'Connexion réussie.');
+                session()->set([
+                    'id' => $client['id'],
+                    'nom' => $client['nom'],
+                    'numero' => $client['numero'],
+                    'isLoggedIn' => true,
+                ]);
+                return redirect()->to('/dashboard');
+            } else {
+                session()->setFlashdata('error', 'Numéro de téléphone introuvable.');
+                return redirect()->to('/login');
+            }
+        } else {
+            session()->setFlashdata('error', 'Numéro de téléphone invalide.');
+            return redirect()->back();
+        }
+    }
+
+    function logout()
+    {
+        session()->destroy();
+        return redirect()->to('/login');
+    }
+}
--- app/Models/ClientModel.php
+++ app/Models/ClientModel.php
@@ -12,7 +12,11 @@ class ClientModel extends Model
     protected $returnType       = 'array';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $allowedFields    = [
+        'id_operateur',
+        'nom',
+        'numero',
+    ];
 
     protected bool $allowEmptyInserts = false;
     protected bool $updateOnlyChanged = true;
